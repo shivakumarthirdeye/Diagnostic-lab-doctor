4a63d1ad7e664bccdc6210b60a7bda52ad0b6ec1
Yogesh <yogesh.kumar.rr5@gmail.com>
2023-05-16 15:11:02 +0530

    pwa setup

--- vite.config.js
+++ vite.config.js
@@ -2,52 +2,62 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react(), VitePWA(manifestForP1ugin)],
-});
-
 const manifestForP1ugin = {
   registrationType: 'prompt',
-  includeAssets: ['./public/favicon.ico', './public/apple-touch-icon.png'],
+  includeAssets: [
+    '/favicon.ico',
+    '/apple-touch-icon.png',
+    '/android-chrome-192x192.png',
+    '/android-chrome-192x192.png',
+    '/manifest-icon-512.maskable.png',
+    '/manifest-icon-192.maskable.png',
+  ],
   manifest: {
     name: 'Diagnostic-lab for Doctor',
     short_name: 'Diagnostic-lab',
     description: 'Diagnostic-lab',
     icons: [
       {
-        src: './public/android-chrome-192x192.png',
+        src: '/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: './public/android-chrome-512x512.png',
+        src: '/android-chrome-192x192.png',
         sizes: '512x512',
         type: 'image/png',
       },
       {
-        src: './public/apple-touch-icon.png',
+        src: '/apple-touch-icon.png',
         sizes: '180x180',
         type: 'image/png',
         purpose: 'apple-touch-icon  ',
       },
       {
-        src: './public/manifest-icon-512.maskable.png',
+        src: '/manifest-icon-512.maskable.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: './public/manifest-icon-192.maskable.png',
+        src: '/manifest-icon-192.maskable.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
     ],
     start_url: '/',
+    scope: '/',
     lang: 'en-US',
     theme_color: '#9A0007',
+    background_color: '#FFFFFF',
     display: 'standalone',
+    orientation: 'portrait',
   },
 };
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react(), VitePWA(manifestForP1ugin)],
+});
